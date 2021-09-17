dccb6fe9729e95435ff078f4e964cd768658da97
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-09-16 21:20:41 -0600

    update animalArray with three items. ? On postman - remembered to include /api/team/

--- Routes/animalRouter.js
+++ Routes/animalRouter.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+let animalArray = [{
+        "id": "1",
+        "animal": "cougar",
+    },
+    {
+        "id": "2",
+        "animal": "dog"
+    },
+    {
+        "id": "3",
+        "animal": "chicken"
+    }, {
+        "id": "4",
+        "animal": "snipe"
+    }
+]
+
+
 
 
 router.get('/', function (req, res) {
-
+    console.log(req);
+    res.send('what is up doc')
     console.log('i am here');
 })
 
